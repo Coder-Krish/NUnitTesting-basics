cd75e9fed7cc9e2400e91bb81bc545ca2abfe984
Krishna Bogati <krishna.bogati@imark.com.np>
2022-08-21 10:40:05 +0545

    setup project

---
+++ Customer.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Customer.Domain.Entities;
+
+namespace Customer.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Customers?> Customers { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ Customer.Domain/Entities/Customers.cs
@@ -0,0 +1,12 @@
+﻿namespace Customer.Domain.Entities
+{
+    public class Customers
+    {
+        public Guid CustomerId { get; private set; }
+        public string CustomerName { get; private set; }
+
+        public string Address { get; private set; }
+
+        public string Email { get; private set; }
+    }
+}
